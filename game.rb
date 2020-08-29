5380d75d33d476962a8c3a67ae98cb66d1f3eec7
Kunal Bhatia <kunalb8607@gmail.com>
2020-08-29 16:42:49 +0000

    updated game.rb

--- game.rb
+++ game.rb
@@ -2,21 +2,21 @@ require_relative 'lib/minesweeper'
 puts '++++++++++++++++++++++++++++'
 puts 'WELCOME TO MINESWEEPER GAME'
 puts '++++++++++++++++++++++++++++'
-puts 'provide value height of board'
 
+print 'provide value height of board: '
 height = gets.chomp.to_i
-
-puts 'provide value width of board'
-
+print "\n"
+print  'provide value width of board: '
 width = gets.chomp.to_i
-
-puts 'provide munber of mines to want to put in board'
-
+print "\n"
+print  'provide munber of mines to want to put in board: '
 num_mines = gets.chomp.to_i
-
+print "\n"
 board = Minesweeper.new(height, width, num_mines)
 puts 'Board before processing'
 board.print_board_preety
+
+puts "==========================================="
 board.calculate_board
 puts 'Board after processing'
 board.print_board_preety
